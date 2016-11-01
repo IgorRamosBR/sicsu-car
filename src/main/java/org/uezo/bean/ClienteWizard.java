8edfac9fe487356eca90a458de0a3c816754a5ba
LucasGarcez <lucasgarcezdev@gmail.com>
2016-10-31 22:20:54 -0200

    cadastro cliente basico

---
+++ src/main/java/org/uezo/bean/ClienteWizard.java
@@ -0,0 +1,53 @@
+package org.uezo.bean;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.event.FlowEvent;
+import org.uezo.model.Cliente;
+
+@ManagedBean
+@ViewScoped
+public class ClienteWizard  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Cliente cliente= new Cliente();
+     
+	    private boolean skip;
+	     
+	    public Cliente getCliente() {
+	        return cliente;
+	    }
+	 
+	    public void setCliente(Cliente cliente) {
+	        this.cliente = cliente;
+	    }
+	     
+	    public void save() {        
+	        FacesMessage msg = new FacesMessage("Cadastrado com Sucesso!", "Bem vindo:" + cliente.getNome());
+	        FacesContext.getCurrentInstance().addMessage(null, msg);
+	    }
+	     
+	    public boolean isSkip() {
+	        return skip;
+	    }
+	 
+	    public void setSkip(boolean skip) {
+	        this.skip = skip;
+	    }
+	     
+	    public String onFlowProcess(FlowEvent event) {
+	        if(skip) {
+	            skip = false;   //reset in case user goes back
+	            return "confirm";
+	        }
+	        else {
+	            return event.getNewStep();
+	        }
+	    }
+
+}
--- src/main/java/org/uezo/model/Cliente.java
+++ src/main/java/org/uezo/model/Cliente.java
@@ -1,15 +1,34 @@
 package org.uezo.model;
 
-public class Cliente {
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
+	//PESSOAIS
 	private String nome;
 	
+	private String sobrenome;
+	
+	private int idade;
+	
+	//CONTATO
 	private String telefone;
 	
-	private String cpf;
-	
 	private String email;
-
+	
+	//ENDERECO
+	
+	private String cidade;
+	
+	private String bairro;
+	
+	private String rua;
+	
+	private String logradouro;
+	
+	//PESSOAIS
 	public String getNome() {
 		return nome;
 	}
@@ -18,20 +37,29 @@ public class Cliente {
 		this.nome = nome;
 	}
 
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	//CONTATO
 	public String getTelefone() {
 		return telefone;
 	}
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
 	}
 
 	public String getEmail() {
@@ -41,6 +69,40 @@ public class Cliente {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	//ENDERECO
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	
 	
 	
 
