5f049a4fa7c57ea8727f4c9199180331d7632fa4
LucasGarcez <lucasgarcezdev@gmail.com>
2016-11-01 15:17:02 -0200

    botao editar

--- src/main/java/org/uezo/bean/ClienteBean.java
+++ src/main/java/org/uezo/bean/ClienteBean.java
@@ -30,15 +30,34 @@ public class ClienteBean {
 	    	List<Cliente> lista = new ArrayList<>();
 	    	
 	    	Cliente cliente1 = new Cliente();
-	    		cliente1.setNome("Nome");
-	    		cliente1.setEmail("email@gmail.com");
-	    		cliente1.setTelefone("99999999");
+	    		//PESSOAL
+	    		cliente1.setNome("Lucas");
+	    		cliente1.setSobrenome("Garcez");
+	    		cliente1.setIdade(20);
+	    		//ENDERECO
+	    		cliente1.setCidade("Rio de Janeiro");
+	    		cliente1.setBairro("Guadalupe");
+	    		cliente1.setRua("Azevedo");
+	    		cliente1.setLogradouro("870");
+	    		//CONTATO
+	    		cliente1.setTelefone("974032234");
+	    		cliente1.setEmail("lucas@gmail.com");
+	    		
 	    	
 	    	Cliente cliente2 = new Cliente();
-	    		cliente2.setNome("Nome");
-	    		cliente2.setEmail("email@gmail.com");
-	    		cliente2.setTelefone("99999999");
-	    	
+		    	//PESSOAL
+	    		cliente2.setNome("Carlos");
+	    		cliente2.setSobrenome("Oliveira");;
+	    		cliente2.setIdade(23);
+	    		//ENDERECO
+	    		cliente2.setCidade("Rio de Janeiro");
+	    		cliente2.setBairro("Campo Grande");
+	    		cliente2.setRua("Manuel Caldeira");
+	    		cliente2.setLogradouro("300");
+	    		//CONTATO
+	    		cliente2.setTelefone("934042265");
+	    		cliente2.setEmail("carlos@gmail.com");
+	    		
 	    	lista.add(cliente1);
 	    	lista.add(cliente2);
 	    	return lista;
