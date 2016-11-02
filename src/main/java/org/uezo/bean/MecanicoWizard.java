d9caab03ed400e00e8808c341ff02235a8db0ccc
tainaramcampos <tainaramcampos@gmail.com>
2016-11-02 18:35:47 -0200

    Cadastro de mecânicos

---
+++ src/main/java/org/uezo/bean/MecanicoWizard.java
@@ -0,0 +1,53 @@
+package main.java.org.uezo.bean;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.event.FlowEvent;
+import main.java.org.uezo.model.Mecanico;
+
+@ManagedBean
+@ViewScoped
+public class MecanicoWizard  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Mecanico mecanico= new Mecanico();
+     
+	    private boolean skip;
+	     
+	    public Mecanico getMecanico() {
+	        return mecanico;
+	    }
+	 
+	    public void setMecanico(Mecanico mecanico) {
+	        this.mecanico = mecanico;
+	    }
+	     
+	    public void save() {        
+	        FacesMessage msg = new FacesMessage("Cadastrado com Sucesso!", "Bem vindo:" + mecanico.getNome());
+	        FacesContext.getCurrentInstance().addMessage(null, msg);
+	    }
+	     
+	    public boolean isSkip() {
+	        return skip;
+	    }
+	 
+	    public void setSkip(boolean skip) {
+	        this.skip = skip;
+	    }
+	     
+	    public String onFlowProcess(FlowEvent event) {
+	        if(skip) {
+	            skip = false;   //reset in case user goes back
+	            return "confirm";
+	        }
+	        else {
+	            return event.getNewStep();
+	        }
+	    }
+
+}
---
+++ src/main/java/org/uezo/model/Mecanico.java
@@ -0,0 +1,127 @@
+package main.java.org.uezo.model;
+
+import java.io.Serializable;
+
+import org.primefaces.component.calendar.Calendar;
+
+public class Mecanico implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	//PESSOAIS
+	private String nome;
+	
+	private String sobrenome;
+	
+	private Calendar dataNascimento;
+	
+	private String cpf;
+	
+	private String especialidade;
+	
+	//CONTATO
+	private String telefone;
+	
+	private String email;
+	
+	//ENDERECO
+	
+	private String cidade;
+	
+	private String bairro;
+	
+	private String rua;
+	
+	private String numero;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	
+	
+	}
