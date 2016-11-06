634b2d80860e2d4954cde356340713cf1c038987
tainaramcampos <tainaramcampos@gmail.com>
2016-11-06 21:32:07 -0200

    cadastro e visualização de peças

---
+++ src/main/java/org/uezo/model/Peca.java
@@ -0,0 +1,47 @@
+package org.uezo.model;
+
+import java.io.Serializable;
+
+public class Peca implements Serializable {
+	
+	private int id;
+
+	private String nome;
+	
+	private float valor;
+	
+	private int quantidade;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
