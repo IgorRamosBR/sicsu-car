38bfdb6cc21ae042e4a8b3a50cbb3f28365c6ab1
LucasGarcez <lucasgarcezdev@gmail.com>
2016-11-06 20:14:54 -0200

    preencher ordem de servico (incompleto)

--- src/main/java/org/uezo/bean/ServicoBean.java
+++ src/main/java/org/uezo/bean/ServicoBean.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import org.uezo.model.Servico;
 
@@ -14,13 +16,15 @@ import org.uezo.model.Servico;
 public class ServicoBean {
 	
 	private List<Servico> servicos;
+	private List<Servico> servicosAdicionados;
 	private Servico servicoSelecionado;
-	
-		private int servicoId;
+	private int servicoId;
 	 
 	    @PostConstruct
 	    public void init() {
 	    	servicos = listaDeServicos();
+	    	servicosAdicionados = new  ArrayList<Servico>();
+	    	servicoSelecionado = new Servico();
 	    }
 	     
 	    public List<Servico> getServicos() {
@@ -56,18 +60,48 @@ public class ServicoBean {
 	    	
 	    	Servico servico1 = new Servico();
 	    		servico1.setId(1);
-	    		servico1.setTipo("Troca de Pneu");
+	    		servico1.setNome("Troca de Pneu");
 	    		servico1.setValor(50);
 	    		
 	    	
 	    	Servico servico2 = new Servico();
 	    		servico2.setId(2);
-	    		servico2.setTipo("Troca de Óleo");
+	    		servico2.setNome("Troca de Óleo");
 	    		servico2.setValor(20);
     		
 	    	lista.add(servico1);
 	    	lista.add(servico2);
 	    	return lista;
 	    }
+		
+		//Metódos para o componente collector
+		
+		public void createNew() {
+			
+	        if(servicosAdicionados.contains(servicoSelecionado)) {
+	            FacesMessage msg = new FacesMessage("Dublicated", "This book has already been added");
+	            FacesContext.getCurrentInstance().addMessage(null, msg);
+	        } 
+	        else {
+	            servicosAdicionados.add(servicoSelecionado);
+	            servicoSelecionado = new Servico();
+	        }
+	    }
+		
+		public String reinit() {
+	        servicoSelecionado = new Servico();
+	         
+	        return null;
+	    }
+
+		public List<Servico> getServicosAdicionados() {
+			return servicosAdicionados;
+		}
+
+		public void setServicosAdicionados(List<Servico> servicosAdicionados) {
+			this.servicosAdicionados = servicosAdicionados;
+		}
+		
+		
 
 }
--- src/main/java/org/uezo/model/Servico.java
+++ src/main/java/org/uezo/model/Servico.java
@@ -4,8 +4,12 @@ import java.io.Serializable;
 
 public class Servico implements Serializable {
 	
-	private int id;
+	private static final long serialVersionUID = 1L;
 
+	private int id;
+	
+	private String nome;
+	
 	private String tipo;
 	
 	private float valor;
@@ -25,6 +29,17 @@ public class Servico implements Serializable {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+	
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		
+		this.nome = nome;
+	}
 
 	public float getValor() {
 		return valor;
@@ -33,5 +48,32 @@ public class Servico implements Serializable {
 	public void setValor(float valor) {
 		this.valor = valor;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Servico other = (Servico) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
 
 }
